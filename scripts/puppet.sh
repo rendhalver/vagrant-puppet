bbafcce386c34f35558e3b17e10fed82be16e97f
Pete Brown <pete@abstractit.com.au>
2016-07-26 00:41:50 +1000

    initial commit

---
+++ scripts/puppet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo 'export PATH="/opt/puppetlabs/bin:$PATH"' > /etc/profile.d/puppet4.sh
+
+echo "Installing Puppet Agent packages..."
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install puppet-agent=$AGENT_VERSION-1trusty
---
+++ scripts/puppet_repo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+echo 'export PATH="/opt/puppetlabs/bin:$PATH"' > /etc/profile.d/puppet4.sh
+
+REPO_NAME="puppetlabs-release-"$COLLECTION"-trusty"
+
+echo "installing colection repo"
+wget http://apt.puppetlabs.com/$REPO_NAME.deb
+dpkg -i $REPO_NAME.deb
+echo "Update APT..."
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update >/dev/null
+
+rm $REPO_NAME.deb
