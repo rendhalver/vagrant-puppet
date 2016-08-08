1ba8094e29992c80f2e4fa1b498c988e27266255
Pete Brown <pete@abstractit.com.au>
2016-08-08 22:29:36 +1000

    use sh -eux for scripts

--- scripts/networking.sh
+++ scripts/networking.sh
@@ -2,7 +2,7 @@
 
 # Disable automatic udev rules for network interfaces in Ubuntu,
 # source: http://6.ptmc.org/164/
-rm -f /etc/udev/rules.d/70-persistent-net.rules;
+rm -rf /etc/udev/rules.d/70-persistent-net.rules;
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
 rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
--- scripts/puppet_repo.sh
+++ scripts/puppet_repo.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh -eux
 
-echo 'export PATH="/opt/puppetlabs/bin:$PATH"' > /etc/profile.d/puppet4.sh
+echo 'export PATH="/opt/puppetlabs/bin:$PATH"' > /etc/profile.d/puppet4.sh;
 
-REPO_NAME="puppetlabs-release-"$COLLECTION"-$OS_NAME"
+REPO_NAME="puppetlabs-release-"$COLLECTION"-$OS_NAME";
 
-echo "installing colection repo"
-wget http://apt.puppetlabs.com/$REPO_NAME.deb
-dpkg -i $REPO_NAME.deb
-echo "Update APT..."
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update >/dev/null
+echo "installing colection repo";
+wget http://apt.puppetlabs.com/$REPO_NAME.deb;
+dpkg -i $REPO_NAME.deb;
+echo "Update APT...";
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update >/dev/null;
 
-rm $REPO_NAME.deb
+rm $REPO_NAME.deb;
